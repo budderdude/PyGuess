f01b2e6f079b57fdd4a44d723aa416eeaf0d5ca8
budderdude <56167982+budderdude@users.noreply.github.com>
2019-10-04 12:58:46 -0400

    Create anotherquestion.py

---
+++ anotherquestion.py
@@ -0,0 +1,21 @@
+print (" this is my first print statement " )
+
+print (""" hello mr ive had to print this out for you
+this is about me wondering if wanted to go to taco bell with me and dipper""")
+
+answer = input("> ")
+
+if answer == "yes": 
+  print(""" you should go to this link to see the results of your journey
+  
+  https://www.quotev.com/story/9553816/Dipper-goes-to-Taco-Bell/2
+  
+  """)
+  
+
+elif answer == "no":
+  print ("""
+  
+  how dare you... HOW DARE YOU...
+  this was going to be very funny but you decided to be very rude
+  """)

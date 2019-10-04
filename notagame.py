e359624ebc0014dc4e01724d6f709db0caa8d213
budderdude <56167982+budderdude@users.noreply.github.com>
2019-10-04 13:04:24 -0400

    Create notagame.py

---
+++ notagame.py
@@ -0,0 +1,21 @@
+# this is going to be really funny is you read this because you actually opened this 
+
+print (" this is my first print statement " )
+
+print (""" hello mr ive had to print this out for you
+this is about me wondering if you want to eat my shorts""")
+
+shorts = input("> ")
+
+if shorts == "yes": 
+  print(""" to know thr full story of your
+  choice go 
+  
+  https://www.quotev.com/story/9553816/Dipper-goes-to-Taco-Bell/2
+  
+  its a nice fan fiction 
+  """)
+  
+
+elif shorts == "no":
+  print (" how dare you, your so really rude so rude")
